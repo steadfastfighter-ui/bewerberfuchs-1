74a0f5570886c7f92345dac438468fb64259ec90
Tarik Ouaissa <Tarik@MacBookPro.fritz.box>
2026-05-16 13:27:44 +0200

    fix cors issue

--- backend/server.js
+++ backend/server.js
@@ -25,9 +25,22 @@ app.use(
   })
 );
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://bewerberfuchs.eu",
+  "https://www.bewerberfuchs.eu",
+  process.env.FRONTEND_URL,
+].filter(Boolean);
+
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Nicht erlaubte CORS Origin"));
+      }
+    },
     credentials: true,
   })
 );
